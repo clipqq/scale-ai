6463d47394e7663c2043e54dcfc57f06ef8a4eb2
clipqq <clifford.l.tran>
2021-09-12 21:33:03 -0700

    testing downloadable json

--- src/app.js
+++ src/app.js
@@ -32,13 +32,10 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 // const slackApp = new App({
 //     signingSecret: process.env.SLACK_SIGNING_SECRET,
 //     token: process.env.SLACK_BOT_TOKEN,
 //   });
-  
-
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common'
 
@@ -80,19 +77,32 @@ app.get('/slackstatus', (req, res) => {
         .then((response) => {
             console.log('got slack response')
             res.send(response)
+            return response
         })
         .catch((error) => {
             res.send(error)
         })
 })
 
+app.get('/color-results.json', (req, res) => {
+    let file = ('./color-results.json')
+    res.download(file)
+})
+
+
 // Endpoint to make API call and write results to JSON file
 app.get('/color', (req, res) => {
     apiHandler
         .makeApiCall()
         .then((response) => {
-            res.json(response)
+            // res.json(response)
             colorTestHandler.colorTest()
+            let output = {
+                response_type: 'in_channel',
+                text: 'Color Test Here: ',
+                attachments: ['https://lit-escarpment-97177.herokuapp.com/color-results.json'],
+            }
+            res.json(output)
         })
         .catch((error) => {
             res.send(error)
@@ -136,6 +146,14 @@ app.post('/post-test', (req, res) => {
                     'https://www.pcgamesn.com/wp-content/uploads/2021/09/knights-of-the-old-republic-remake-pc-580x334.jpg',
             },
         ],
+    }
+    res.json(data)
+})
+
+app.post('/slack-status', (req, res) => {
+    let data = {
+        response_type: 'in_channel',
+        text: `Here's the status:${status}`,
     }
     res.json(data)
 })
